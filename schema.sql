e08fd871204d39dc6e5f1c58c74162341ad74287
PNdunguMaina <ndunguwamaina@gmail.com>
2022-09-27 16:09:47 +0300

    Create table named animals bearing six columns

--- schema.sql
+++ schema.sql
@@ -1,5 +1,8 @@
-/* Database schema to keep the structure of entire database. */
-
 CREATE TABLE animals (
-    name varchar(100)
+    id int not null,
+    name char not null,
+    date_of_birth date,
+    escape_attempts int,
+    neutered bit,
+    weight_kg decimal
 );
